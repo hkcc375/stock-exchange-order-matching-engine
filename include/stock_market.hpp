eba3d35b7fe0dbb301dcbfa2a2ab7e33c596fb94
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2025-09-21 10:20:21 +0530

    Add OrderBook class

--- include/stock_market.hpp
+++ include/stock_market.hpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <chrono>
 #include <ctime>
+#include <map>
+#include <queue>
+
 
 namespace Components {
 
@@ -12,6 +15,7 @@ namespace Components {
     enum class OrderStatus {PLACED, PARTIALLY_FILLED, FILLED, CANCELLED};
 
     class Order {
+        // Properties of an Order placed;
         private:
             int orderID;
             int userID;
@@ -24,6 +28,7 @@ namespace Components {
     };
 
     class Trade {
+        // Properties of a successful Trade;
         private:
             int tradeID;
             int buyOrderID;
@@ -34,6 +39,26 @@ namespace Components {
         public:
     };
 
+    class TradeBook {
+        // Maintains all successful trades recorded by the system;
+        private:
+            vector<Trade> trades;
+        public:
+            void displayAllTrades() const;
+            void recordTrade(Trade& t);
+            void queryTrade(Trade& t) const;
+    };
+
+    class OrderBook {
+        private:
+            // Price -> Queue of Orders (ordered by Timestamp);
+            map<double, queue<Order>> buyOrders;
+            map<double, queue<Order>> sellOrders;
+        public:
+            void matchOrders();
+            void processNewOrder(const Order& o);
+            void cancelOldOrder(const Order& o); 
+    };
 }
 
 #endif
